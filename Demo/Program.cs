01b51a574a2fe54b98094530fd19d11395009408
Narenthra Kishore RS <narenthrakishore.rs@tvsnext.com>
2023-07-10 14:27:07 +0530

    [Narenthra Kishore] add: Swap program

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,12 +1,33 @@
-﻿namespace Demo
+﻿using System.Transactions;
+
+namespace Demo
 {
     class Demo
 
     {
         public static void Main()
         {
-            Table.table();
+            bool flag = true;
+            while (flag)
+            {
+                int input  = Convert.ToInt32(Console.ReadLine());
+                switch (input)
+                {
+                    case 1:
+                        Table.table();
+                        break;
+                    case 2:
+                        Swap.swap();
+                        break;
+                    default: 
+                        flag = false;
+                        break;
+                }
+            }
+
             
         }
     }
 }
+
+
---
+++ Demo/Swap.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    internal class Swap
+    {
+        public static void swap()
+        {
+            Console.WriteLine("Enter the 2 values");
+
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            a = a + b;
+            b=a-b;
+            a= a - b;
+
+            Console.WriteLine(a+" "+b);
+
+
+
+        }
+    }
+}
