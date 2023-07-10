7887f406803ebb19a4adb3d7419956518492be5c
Narenthra Kishore RS <narenthrakishore.rs@tvsnext.com>
2023-07-10 15:40:28 +0530

    [Narenthra kishore] Add: output line

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -8,6 +8,8 @@ namespace Demo
         public static void Main(string[] args)
         {
             bool flag = true;
+            Console.WriteLine("Enter the value");
+            Console.WriteLine("1.table\n2.Swap\n3.Vowels count\n4.exit");
             while (flag)
             {
                 int input  = Convert.ToInt32(Console.ReadLine());
